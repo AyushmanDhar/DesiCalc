e67a0e3e2ee72c78d2d2adc1020591c30e4152bc
Ayushman Dhar <dharayushman@gmail.com>
2026-07-05 15:56:42 +0530

    fix: ads not rendering due to race condition with Adsterra responsive.js
    Adsterra's responsive.js calls affilistStart() only on window.load, but
    ad.js lazily injects ad tags via requestIdleCallback (up to 2s delay),
    by which time window.load has already fired. Fix: remove inline script
    tags from each ad slot, load responsive.js once after injection, and
    call affilistStart() directly via onload callback.

--- public/assets/js/ad.js
+++ public/assets/js/ad.js
@@ -1,13 +1,13 @@
 (function() {
   var AD_TAGS = {
-    sidebar_top_income_tax: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="ic4c1ef42c4" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/c4c1ef42c4/?placementName=sidebar_top_income_tax"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    in_content_income_tax: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="k167215d9e9" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/167215d9e9/?placementName=in_content_income_tax"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    sidebar_top_stamp_duty: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="a5b24fbeab2" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/5b24fbeab2/?placementName=sidebar_top_stamp_duty"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    in_content_stamp_duty: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="naf2187c044" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/af2187c044/?placementName=in_content_stamp_duty"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    sidebar_top_rto_tax: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="v9e1ddb597f" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/9e1ddb597f/?placementName=sidebar_top_rto_tax"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    in_content_rto_tax: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="m325bd96716" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/325bd96716/?placementName=in_content_rto_tax"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    in_content_home: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="y6e7256b01f" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/6e7256b01f/?placementName=in_content_home"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>',
-    footer_main: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="h894273e566" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/894273e566/?placementName=footer_main"><script src="//data527.click/js/responsive.js" async><\/script><\/ins>'
+    sidebar_top_income_tax: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="ic4c1ef42c4" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/c4c1ef42c4/?placementName=sidebar_top_income_tax"></ins>',
+    in_content_income_tax: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="k167215d9e9" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/167215d9e9/?placementName=in_content_income_tax"></ins>',
+    sidebar_top_stamp_duty: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="a5b24fbeab2" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/5b24fbeab2/?placementName=sidebar_top_stamp_duty"></ins>',
+    in_content_stamp_duty: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="naf2187c044" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/af2187c044/?placementName=in_content_stamp_duty"></ins>',
+    sidebar_top_rto_tax: '<ins style="width:300px;height:250px" data-width="300" data-height="250" class="v9e1ddb597f" data-domain="//data527.click" data-affquery="/a0699d3fbee38a6e7d3e/9e1ddb597f/?placementName=sidebar_top_rto_tax"></ins>',
+    in_content_rto_tax: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="m325bd96716" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/325bd96716/?placementName=in_content_rto_tax"></ins>',
+    in_content_home: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="y6e7256b01f" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/6e7256b01f/?placementName=in_content_home"></ins>',
+    footer_main: '<ins style="width:728px;height:90px" data-width="728" data-height="90" class="h894273e566" data-domain="//data527.click" data-affquery="/3cd431ceb2117805f8a8/894273e566/?placementName=footer_main"></ins>'
   };
 
   var PAGE_SLOTS = {
@@ -25,6 +25,34 @@
     return 'index';
   }
 
+  function triggerAdRendering() {
+    if (typeof window.affilistStart === 'function') {
+      window.affilistStart();
+      return;
+    }
+    var tries = 0;
+    var check = setInterval(function() {
+      if (typeof window.affilistStart === 'function') {
+        clearInterval(check);
+        window.affilistStart();
+      } else if (++tries > 50) {
+        clearInterval(check);
+      }
+    }, 100);
+  }
+
+  function loadAdScript() {
+    if (document.querySelector('script[src*="data527.click/js/responsive.js"]')) {
+      triggerAdRendering();
+      return;
+    }
+    var s = document.createElement('script');
+    s.src = '//data527.click/js/responsive.js';
+    s.async = true;
+    s.onload = triggerAdRendering;
+    document.head.appendChild(s);
+  }
+
   function injectAds() {
     var pageKey = getPageKey();
     var slots = PAGE_SLOTS[pageKey] || [];
@@ -38,6 +66,7 @@
         }
       }
     });
+    loadAdScript();
   }
 
   if ('requestIdleCallback' in window) {
